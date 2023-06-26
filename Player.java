242e7d3eb47b1b62d8a2283225c7efb74c51835f
Manu <progamerro12345@gmail.com>
2023-06-26 12:01:48 +0300

    Encrypted save file

--- Player.java
+++ Player.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.NoSuchElementException;
@@ -31,13 +28,26 @@ public class Player {
 
     public void loadProgress () {
         try {
-            Scanner scanner = new Scanner(fila);
+            File temp = File.createTempFile("TempFile", "txt");//creaza fisier temp care nu ii "criptat"
+            Scanner scanner = new Scanner(temp);
+            Encoder bitcoin = new Encoder();
 
-            clicks = Float.parseFloat(scanner.next());
-            clickPower = Float.parseFloat(scanner.next());
-            pretu = Integer.parseInt(scanner.next());
-            scanner.nextLine();
-            upgradeuri = scanner.nextLine();
+            try {//daca se schimba ceva prin fisier => restart progress
+            FileWriter fr = new FileWriter(temp);
+            fr.write(bitcoin.decryptingFile(fila));
+            fr.close();
+
+                clicks = Float.parseFloat(scanner.next());
+                clickPower = Float.parseFloat(scanner.next());
+                pretu = Integer.parseInt(scanner.next());
+                scanner.nextLine();
+                upgradeuri = scanner.nextLine();
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException | NoSuchElementException e) {
+                Player player = new Player();
+                player.save();
+            }
+
+            temp.delete();
 
             DecimalFormatSymbols symbols = new DecimalFormatSymbols();//citeste doubles si le rotunjeste
             symbols.setDecimalSeparator('.');
@@ -47,24 +57,21 @@ public class Player {
             clicks = Double.parseDouble(roundedNumber);
 
             scanner.close();
-        } catch (NoSuchElementException e) {//daca ceva nu e bine la fila (a fost modificata gresit) se reseteaza tot
-            Player player = new Player();
-            player.save();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void save () {
+        Encoder bitcoin = new Encoder();
         String salvare = clicks + "\n" + clickPower + "\n" + pretu + "\n." + upgradeuri;
+
         try {
-            fila.setWritable(true);
             FileWriter fr = new FileWriter(fila);
-            fr.write(salvare);
+            fr.write(bitcoin.cryptingText(salvare));
             fr.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        fila.setReadOnly();
     }
 }
