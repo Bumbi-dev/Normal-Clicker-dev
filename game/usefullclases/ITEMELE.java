887e5ceebf8dcb267f60817ccd21ed91f9eb6a05
Manu <progamerro12345@gmail.com>
2023-07-24 22:05:13 +0300

    Trying to implement a class with all the items

---
+++ game/usefullclases/ITEMELE.java
@@ -0,0 +1,10 @@
+package game.usefullclases;
+
+import game.gameplay.Item;
+
+public interface ITEMELE {//help me god
+    Item rights = null;
+    Item moreRights = null;
+    Item scam = null;
+    Item []upgradelist = {rights, moreRights, scam};
+}
